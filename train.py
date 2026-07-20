5ba238326914c8d6854543344fc7f267fbde834e
Zhai <mzhai72@splprhpc08.cm.cluster>
2026-07-20 16:29:38 -0500

    training run 4 completed

--- train.py
+++ train.py
@@ -7,8 +7,8 @@ model.train(
     data=str(Path(__file__).parent / "data.yaml"),
     epochs=100,
     imgsz=3200,
-    batch=16,       # 1280px tiles use ~4x more VRAM than 640px; tune up if GPU allows
-    device=[0,1],  # use 1 gpu
+    batch=16,       # 3200 tiles use ~16x more VRAM than 640px; tune up if GPU allows
+    device=[0,1],  # use 2 gpu
     project="3200px_training_v4",
     name="cfu_detector",
     exist_ok=True,
